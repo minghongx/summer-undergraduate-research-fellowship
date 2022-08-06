40881a72395e9cf9040c3e22d5e849fce55de23e
Minghong Xu <86758413+MinghongAlexXu@users.noreply.github.com>
2022-08-06 14:16:49 +0000

    Fix typos in ModelSettings.h
    pinocchioInterfacePtr_->getModel().nq at line 119 of
    LeggedRobotInterface.cpp restored to desired value, 18, thereby
    constructing abit accurate centroidalModelInfo_ and returning correct
    qJoints at line 125 of LeggedRobotVisualizer.cpp.

--- go1/include/go1/ModelSettings.h
+++ go1/include/go1/ModelSettings.h
@@ -23,7 +23,7 @@ struct ModelSettings {
   // This is only used to get names for the knees and to check urdf for extra joints that need to be fixed.
 //   std::vector<std::string> jointNames{"LF_HAA", "LF_HFE", "LF_KFE", "RF_HAA", "RF_HFE", "RF_KFE",
 //                                       "LH_HAA", "LH_HFE", "LH_KFE", "RH_HAA", "RH_HFE", "RH_KFE"};
-  std::vector<std::string> jointNames{"FL_hip_joint:", "FL_thigh_joint:", "FL_calf_joint", "FR_hip_joint", "FR_thigh_joint", "FR_calf_joint", "RL_hip_joint", "RL_thigh_joint", "RL_calf_joint", "RR_hip_joint", "RR_thigh_joint", "RR_calf_joint"};
+  std::vector<std::string> jointNames{"FL_hip_joint", "FL_thigh_joint", "FL_calf_joint", "FR_hip_joint", "FR_thigh_joint", "FR_calf_joint", "RL_hip_joint", "RL_thigh_joint", "RL_calf_joint", "RR_hip_joint", "RR_thigh_joint", "RR_calf_joint"};
   std::vector<std::string> contactNames6DoF{};
 //   std::vector<std::string> contactNames3DoF{"LF_FOOT", "RF_FOOT", "LH_FOOT", "RH_FOOT"};
   std::vector<std::string> contactNames3DoF{"FL_foot", "FR_foot", "RL_foot", "RR_foot"};
